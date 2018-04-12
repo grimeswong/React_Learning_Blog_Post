1af9bf6e74b29e413d88d4ff28278eacfb5ae006
Shadow Grimes <grimeswong@gmail.com>
2018-04-13 07:10:51 +1000

    135: Modified the Fields and renderField function helper that generate the input field with DRY

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';  // reduxForm function helper - wrap the postsNew component that communicate directly from this component to the reducer
 
 class PostsNew extends Component {
-  renderTitleField(field) { //  contain event handler is going to be responsible for making sure this field
+  renderField(field) { //  contain event handler is going to be responsible for making sure this field
     return(
-      <div>
-        <input
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control"
           type="text"
           {...field.input}
         />
@@ -17,13 +18,23 @@ class PostsNew extends Component {
     );
   }
 
-
   render() {
     return(
       <form>
         <Field
+          label="Title For Post"
           name="title"
-          component={this.renderTitleField}
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="tag"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
         />
       </form>
     );
