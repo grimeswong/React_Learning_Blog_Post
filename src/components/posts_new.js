86a0651b0616e5a90ebff9aa4abaa734353dbba6
Shadow Grimes <grimeswong@gmail.com>
2018-04-13 06:59:25 +1000

    134: Added Field and reduxForm help to display and wire up the form with input field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';  // reduxForm function helper - wrap the postsNew component that communicate directly from this component to the reducer
 
 class PostsNew extends Component {
+  renderTitleField(field) { //  contain event handler is going to be responsible for making sure this field
+    return(
+      <div>
+        <input
+          type="text"
+          {...field.input}
+        />
+        {/* equal below few lines */}
+        {/* onChange={field.input.onChange }*/}
+        {/* onFocus={field.input.onFocus }*/}
+        {/* onBlur={field.input.onBlur }*/}
+      </div>
+    );
+  }
+
+
   render() {
     return(
       <form>
         <Field
           name="title"
-          component={}
+          component={this.renderTitleField}
         />
       </form>
     );
@@ -15,5 +31,5 @@ class PostsNew extends Component {
 }
 
 export default reduxForm({  // configuration:
-  form: 'PostsNewForm'    // must be unique
+  form: 'PostsNewForm'    // must be unique that not share the state with other form
 })(PostsNew);
