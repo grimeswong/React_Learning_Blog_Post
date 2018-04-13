7a3f3364e0f3de77a6b583d569e3e3fc24026f23
Shadow Grimes <grimeswong@gmail.com>
2018-04-14 03:12:17 +1000

    150: Added Delete post to reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) { //default state to an object
   switch (action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload); //if the state object has a key of the post id, just drop it
     case FETCH_POST:
-    console.log("Fetching Single Post..."); // debugger:
+      console.log("Fetching Single Post..."); // debugger:
 
     /* ES5 Syntax */
     // const post = action.payload.data;
